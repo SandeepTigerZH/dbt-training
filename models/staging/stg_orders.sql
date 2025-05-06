032bfee66c6ec055adb4c487384a4ac57de4bd19
dobriyalmuskan@gmail.com <dobriyalmuskan@gmail.com>
2025-05-06 07:13:22 +0000

    added delivery_team seed and added to the stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select
 --from raw_orders
+{{ dbt_utils.surrogate_key(['orderid', 'o.customerid', 'o.productid']) }} as sk_orders,
 o.orderid,
 o.orderdate,
 o.shipdate,
@@ -15,9 +16,12 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup('ordersellingprice','ordercostprice') }} as markup
+{{ markup('ordersellingprice','ordercostprice') }} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ 'delivery_team'}} as d
+on o.shipmode = d.shipmode
